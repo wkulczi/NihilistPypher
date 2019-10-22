38534982a61eebd674c31eb6d1cbfe0bee876051
Wojciech Kulczak <wojciech.kulczak@apilia.pl>
2019-10-22 21:14:51 +0200

    divided front and back into classes, now gotta connect that

--- BackClass.py
+++ BackClass.py
@@ -1,13 +1,113 @@
+from unidecode import unidecode
+import pandas as pd
+
 class Back:
-    def __init__(self, key, martix_word, cypher_word, substitute_choice):
+    def __init__(self):
         self.alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
                          "t", "u",
                          "v", "w",
                          "x", "y", "z"]
-        self.key = key
-        self.substitutes = []
-        self.word = martix_word
-        self.cypher_word = cypher_word
-        self.substitute_choice = substitute_choice
+        self.key = ""
+        self.substitute_symbol = ("v", "w")
+        self.fromfile = bool()
+        self.word = ""
+        self.cypher_word = ""
+        self.matrix = [[]]
+        self.matrix_parsed=""
+        self.cyphered_key=[]
+        self.cyphered_word=""
 
+    def setFromfile(self, x):
+        self.fromfile = x
 
+    def setSubstitute(self, sub):
+        self.substitute_symbol = sub
+
+    def substituteLetters(self, word):
+        return [self.substitute_symbol[1] if x == self.substitute_symbol[0] else x for x in word]
+
+    def removeDuplicates(self, word):
+        return list(dict.fromkeys(word))
+
+    def findlocation(self, matrix, char):
+        for row in range(len(matrix)):
+            for column in range(len(matrix[0])):
+                if matrix[row][column] == char:
+                    return str(row + 1) + str(column + 1)
+
+    def prepare(self, x, is_alphabet):
+        x = [x.lower() for x in x]
+        x = ''.join(filter(str.isalpha, x))
+        x = unidecode(x)
+        x = self.substituteLetters(x)
+
+        if is_alphabet:
+            x = self.removeDuplicates(x)  # cos zes tu logicznie spieprzyl, drzemka i popraw
+            return [x[0:5], x[5:10], x[10:15], x[15:20], x[20:25]]
+        return x
+
+    def formatMatrix(self,matrix):
+        mat=pd.DataFrame(matrix,columns=[1,2,3,4,5],index=[1,2,3,4,5])
+        mat=mat.to_string()
+        self.matrix_parsed=mat
+
+    def cipher(self, key, matrixword, word):
+        self.key = self.prepare(key, False)
+        self.matrix = self.prepare((list(matrixword) + self.alphabet), True)
+        self.word = self.prepare(word, False)
+        print(self.substitute_symbol)
+        self.formatMatrix(self.matrix)
+        x: str
+        ciphered_message = list()
+        ciphered_key = list()
+        for x in self.word:
+            ciphered_message.insert(len(ciphered_message), int(self.findlocation(self.matrix, x)))
+        for x in self.key:
+            ciphered_key.insert(len(ciphered_key), int(self.findlocation(self.matrix, x)))
+        self.cyphered_key=ciphered_key
+        self.cyphered_word=ciphered_message
+        nihi_list = list()
+        for x in range(0, len(ciphered_message)):
+            nihi_list.insert(len(nihi_list), (ciphered_message[x] + ciphered_key[x % len(ciphered_key)]) % 100)
+        for x in range(0, len(nihi_list)):
+            if nihi_list[x] < 10:
+                nihi_list[x] = "0" + str(nihi_list[x])
+            else:
+                nihi_list[x] = str(nihi_list[x])
+        return nihi_list
+
+    def decypherWord(self, matrix, word):
+        try:
+            buffer = ""
+            for x in range(0, len(word)):
+                buffer += matrix[int(str(word[x])[0]) - 1][int(str(word[x])[1]) - 1]
+            return buffer
+        except:
+            return "WRONG KEY AND/OR SUBSTITUTE.\nLETTERS OUT OF BOUNDS"
+
+    def decipher(self, key, cyph_word, matrixword):
+        self.matrix = self.prepare((list(matrixword) + self.alphabet), True)
+        self.key = self.prepare(key, False)
+        print(self.substitute_symbol)
+        print(self.matrix)
+        print(self.key)
+        cyph_key = list()
+        decyph_location = list()
+        cyph_word = cyph_word.split()
+        for x in range(0, len(cyph_word)):
+            y = int(cyph_word[x])
+            if y < 10:
+                y += 100
+            cyph_word[x] = y
+        self.formatMatrix(self.matrix)
+        for x in self.key:
+            cyph_key.insert(len(cyph_key), int(self.findlocation(self.matrix, x)))
+
+        self.cyphered_key = cyph_key
+        self.cyphered_word =  cyph_word
+        for x in range(0, len(cyph_word)):
+            decyph_location.insert(len(decyph_location),cyph_word[x] - cyph_key[x % len(cyph_key)])  ##[aa,bb,cc...] po odjęciu
+        print(cyph_key)
+        print(decyph_location)
+        decyph_word = self.decypherWord(self.matrix, decyph_location)
+        return decyph_word
--- FrontClass.py
+++ FrontClass.py
@@ -1,32 +1,140 @@
 from tkinter import *
+from tkinter import filedialog
 from tkinter.scrolledtext import ScrolledText
-
+import BackClass
 
 # self.scroll_txt_list[0].insert(INSERT, "hello")
+from Util import isdefined
+
 
 class Front:
-    def _hello(self):
-        self.str = self.scroll_txt_list[0].get('1.0', END)
-        print(self.str)
+    def cipher(self):
+        text = self.scroll_txt_list[0].get('1.0', END).strip()
+        key = self.txt_list[1].get('1.0', END).strip()
+        matrixword = self.txt_list[0].get('1.0', END).strip()
+        filepath = ""
+        if self.choice_source==1:
+            self.scroll_txt_list[1].configure(state="normal")
+            self.scroll_txt_list[0].configure(state="normal")
+        if isdefined(key) and (isdefined(text) or isdefined(filepath)):
+            wynik=self.backbone.cipher(key,matrixword,text) #print to textfile
+            print(wynik)
+            self.scroll_txt_list[1].configure(state="normal")
+            self.scroll_txt_list[1].delete('1.0',END)
+            self.scroll_txt_list[1].insert(END, wynik)
+            self.scroll_txt_list[1].configure(state="disabled")
+        else:
+            if not isdefined(key):
+                self.txt_list[1].insert('1.0',"KEY MISSING, INSERT KEY")
+            if not isdefined(text):
+                self.scroll_txt_list[0].insert('1.0', "INSERT WORD TO CIPHER")
+    def decipher(self):
+        text = self.scroll_txt_list[0].get('1.0', END).strip()
+        key = self.txt_list[1].get('1.0', END).strip()
+        matrixword = self.txt_list[0].get('1.0', END).strip()
+        filepath = ""
+        if isdefined(key) and (isdefined(text) or isdefined(filepath)):
+            wynik=self.backbone.decipher(key,text,matrixword)
+            print(wynik)
+            self.scroll_txt_list[1].configure(state="normal")
+            self.scroll_txt_list[1].delete('1.0',END)
+            self.scroll_txt_list[1].insert(END, wynik)
+            self.scroll_txt_list[1].configure(state="disabled")
+        else:
+            print("ye dummfuk i need data to cypher")
+    # trzeba zrobic jakiegos latcha, zeby settowal czy matrixword/file jest zdefiniowane, zeby nie szyfrowal pustki
+    # self.backbone.cipher
 
-    def setChoice(self):
-        print(self.x.get())
-        print(self.choice_list[self.x.get()])
+    def setChoiceSource(self):  # 0=text 1=file
+        self.choice_source = self.y.get()
+        self.backbone.setFromfile(self.choice_source)
+        if self.choice_source==1:
+            self.scroll_txt_list[0].configure(state="normal")
+            self.scroll_txt_list[0].delete('1.0',END)
+            self.scroll_txt_list[0].insert(END, "Set file path.")
+            self.scroll_txt_list[1].configure(state="disabled")
+            self.scroll_txt_list[0].configure(state="disabled")
+        else:
+            self.scroll_txt_list[1].configure(state="normal")
+            self.scroll_txt_list[0].configure(state="normal")
 
+    def setChoiceSub(self):
+        self.choice_sub = self.choice_list[self.x.get()]
+        self.backbone.setSubstitute(self.choice_sub)
+    def generategrids(self):
+        if isdefined(self.backbone.matrix_parsed):
+            self.scroll_txt_list[2].configure(state="normal")
+            self.scroll_txt_list[2].delete('1.0', END)
+            self.scroll_txt_list[2].insert(END, self.backbone.matrix_parsed)
+            self.scroll_txt_list[2].insert(END, "\n TRANSLATED WORD: \n ")
+            self.scroll_txt_list[2].insert(END, self.backbone.cyphered_word)
+            self.scroll_txt_list[2].insert(END, "\n TRANSLATED KEY: \n ")
+            self.scroll_txt_list[2].insert(END, self.backbone.cyphered_key)
+            self.scroll_txt_list[2].configure(state="disabled")
+
+        else:
+            self.scroll_txt_list[2].configure(state="normal")
+            self.scroll_txt_list[2].delete('1.0', END)
+            if not isdefined(self.txt_list[1].get('1.0', END).strip()) and not isdefined(self.scroll_txt_list[0].get('1.0', END).strip()):
+                self.scroll_txt_list[2].insert(END, "NO GRIDS FOUND. INSERT KEY AND WORDS TO CIPHER.")
+            elif not isdefined(self.scroll_txt_list[0].get('1.0', END).strip()):
+                self.scroll_txt_list[2].insert(END, "NO GRIDS FOUND. INSERT WORDS TO CIPHER.")
+            elif not isdefined(self.txt_list[1].get('1.0', END).strip()):
+                self.scroll_txt_list[2].insert(END, "NO GRIDS FOUND. INSERT KEY.")
+            else:
+                self.scroll_txt_list[2].insert(END, "PRESS CYPHER/DECYPHER BUTTON FIRST.")
+            self.scroll_txt_list[2].configure(state="disabled")
+
+    def setfilepath(self):
+        if self.choice_source==1:
+            self.filepath=filedialog.askopenfilename(initialdir="/", title="Select file to cipher", filetypes= (("txt files","*.txt"),("all files","*.*")))
+            try:
+                file=open(self.filepath,"r")
+                fromfile=file.read()
+                self.scroll_txt_list[0].configure(state="normal")
+                self.scroll_txt_list[0].delete('1.0', END)
+                self.scroll_txt_list[0].insert('1.0', fromfile)
+                self.scroll_txt_list[0].configure(state="disabled")
+            except:
+                self.scroll_txt_list[0].configure(state="normal")
+                self.scroll_txt_list[0].delete('1.0', END)
+                self.scroll_txt_list[0].insert('1.0', "NO FILE AT: \""+self.filepath+"\" FOUND.")
+                self.scroll_txt_list[0].configure(state="disabled")
+        else:
+            self.scroll_txt_list[0].configure(state="normal")
+            self.scroll_txt_list[0].delete('1.0', END)
+            self.scroll_txt_list[0].insert('1.0', "Change to \"From file\" first")
+            self.scroll_txt_list[0].configure(state="disabled")
+    def savetofile(self):
+        self.scroll_txt_list[1].configure(state="normal")
+        cypheredmatrix=self.scroll_txt_list[1].get('1.0', END).strip()
+        if isdefined(cypheredmatrix):
+            self.savefilepath=filedialog.asksaveasfilename(initialdir = "/",title = "Select file",filetypes = (("txt files",".txt"),("all files","*.*")))
+            self.savefilepath=self.savefilepath+".txt"
+            savefile=open(self.savefilepath,"w+")
+            savefile.write(cypheredmatrix)
+        else:
+            self.scroll_txt_list[1].insert('1.0',"NO CYPHERED TEXT FOUND.")
+        self.scroll_txt_list[1].configure(state="disabled")
     def __init__(self):
+        self.filepath=""
         self.root = Tk()
         self.root.title("NihilistPypher main")
+        self.root.geometry("900x600+200+200")
         self.root.geometry("900x600+200+200")
         self.root.resizable(FALSE, FALSE)
         self.x = IntVar()
         self.y = IntVar()
         self.str = ""
-        self.choice_list = [("v", "w"), ("w","v"),("c", "k"), ("k", "c"), ("j", "i"), ("i", "j")]
+        self.choice_list = [("v", "w"), ("w", "v"), ("c", "k"), ("k", "c"), ("j", "i"), ("i", "j")]
         self.button_list = self.setButtonList()
         self.scroll_txt_list = self.setSTxtList()
         self.txt_list = self.setTxtList()
         self.label_list = self.setLabelList()
         self.radio_list = self.setRadioList()
+        self.choice_sub = []
+        self.choice_source = int()
+        self.backbone = BackClass.Back()
 
     def setSTxtList(self):
         ST = [ScrolledText(self.root, width=50, height=13, wrap=WORD),
@@ -45,19 +153,23 @@ class Front:
         return L
 
     def setRadioList(self):
-        R = [Radiobutton(self.root, text="v<=>w", padx=20, variable=self.x, command=self.setChoice, value=0),
-             Radiobutton(self.root, text="w<=>v", padx=20, variable=self.x, command=self.setChoice, value=1),
-             Radiobutton(self.root, text="c<=>k", padx=20, variable=self.x, command=self.setChoice, value=2),
-             Radiobutton(self.root, text="k<=>c", padx=20, variable=self.x, command=self.setChoice, value=3),
-             Radiobutton(self.root, text="j<=>i", padx=20, variable=self.x, command=self.setChoice, value=4),
-             Radiobutton(self.root, text="From text area", padx=20, variable=self.y, value=0),
-             Radiobutton(self.root, text="From file", padx=20, variable=self.y, value=1),
-             Radiobutton(self.root, text="i<=>j", padx=20, variable=self.x, command=self.setChoice, value=5)]
+        R = [Radiobutton(self.root, text="v<=>w", padx=20, variable=self.x, command=self.setChoiceSub, value=0),
+             Radiobutton(self.root, text="w<=>v", padx=20, variable=self.x, command=self.setChoiceSub, value=1),
+             Radiobutton(self.root, text="c<=>k", padx=20, variable=self.x, command=self.setChoiceSub, value=2),
+             Radiobutton(self.root, text="k<=>c", padx=20, variable=self.x, command=self.setChoiceSub, value=3),
+             Radiobutton(self.root, text="j<=>i", padx=20, variable=self.x, command=self.setChoiceSub, value=4),
+             Radiobutton(self.root, text="From text area", padx=20, variable=self.y, command=self.setChoiceSource,
+                         value=0),
+             Radiobutton(self.root, text="From file", padx=20, variable=self.y, command=self.setChoiceSource, value=1),
+             Radiobutton(self.root, text="i<=>j", padx=20, variable=self.x, command=self.setChoiceSub, value=5)]
         return R
 
     def setButtonList(self):
-        B = [Button(self.root, text="Code", command=self._hello), Button(self.root, text="Decode", command=self._hello),
-             Button(self.root, text="Generate grids", command=self._hello)]
+        B = [Button(self.root, text="Code", command=self.cipher),
+             Button(self.root, text="Decode", command=self.decipher),
+             Button(self.root, text="Generate grids", command=self.generategrids),
+             Button(self.root, text="Import file", command=self.setfilepath),
+             Button(self.root, text="Export to file", command=self.savetofile)]
         return B
 
     def run(self):
@@ -83,3 +195,8 @@ class Front:
         self.button_list[0].place(x=200, y=160)
         self.button_list[1].place(x=200, y=190)
         self.button_list[2].place(x=20, y=280)
+        self.button_list[3].place(x=364, y=100)
+        self.button_list[4].place(x=360, y=420)
+
+
+        #jak zrobi from file to zablokuj inputy
